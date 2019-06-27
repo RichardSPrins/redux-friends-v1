2f4121037d985b66143d45c5405af958d2a53caa
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-06-27 14:55:07 -0700

    reducers and actions complete

--- friends/src/index.js
+++ friends/src/index.js
@@ -6,7 +6,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import reducer from './reducers'
 
-const store = createStore(() => {}, applyMiddleware(thunk))
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(<Provider store={store}>< App /> </Provider>,document.getElementById('root'));
