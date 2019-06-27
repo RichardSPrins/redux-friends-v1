fe140282e6cc4864a0a1157ceaabb05f7a82aa05
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-06-27 15:45:53 -0700

    working on axios requests

--- friends/src/components/FriendsListView.js
+++ friends/src/components/FriendsListView.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import FriendsList from './FriendsList';
+import { getFriends } from '../actions';
+import { connect } from 'react-redux'
+
 
 
 class FriendsListView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      
-     };
-  }
   render() {
     return (
       <FriendsList />
@@ -16,4 +13,8 @@ class FriendsListView extends Component {
   }
 }
 
-export default FriendsListView;
+const mapStateToProps = state => ({
+  isFetching: state.isFetching,
+  friends: state.friends,
+})
+export default connect(mapStateToProps, {getFriends})(FriendsListView);
